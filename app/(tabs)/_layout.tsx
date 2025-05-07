269228fcf38cd8667c8165b81b72bf170347e7f8
stucci <sts.ucd@gmail.com>
2025-05-08 01:20:34 +0200

    Update eas.json to include latest images for Android and iOS; modify tab title to 'Home (dev)' in TabLayout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -21,7 +21,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Home (dev)',
           tabBarIcon: ({ color, focused }) => (
             <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
           ),
